c66a1f7548befb37dfcfaebf5008a74ccf2bcb20
KKrishna11 <kushwaha.krishna1233@gmail.com>
2022-12-01 23:37:15 +0530

    lolfinallyadd

---
+++ blog/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'bloginapp.components file';
+  name='KRISHNA'
+  getName(){
+    return this.name
+  }
+  obj={
+    name:"ThisIsObjectValue",
+    age:19
+  }
+
+  arr=['NeymarArray','Messi']
+}
